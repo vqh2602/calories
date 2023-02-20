37c106358609b5f7468909b2fd8abd7355b5c832
vuong quang huy <vqh2602@gmail.com>
2023-02-20 18:44:29 +0700

    base v4

--- lib/modules/dashboard/dashboard_screen.dart
+++ lib/modules/dashboard/dashboard_screen.dart
@@ -1,8 +1,10 @@
 import 'package:calories/widgets/base/base.dart';
+import 'package:calories/widgets/image_custom.dart';
 import 'package:calories/widgets/text_custom.dart';
 import 'package:calories/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:getwidget/getwidget.dart';
 
 class DashboardScreen extends StatefulWidget {
   const DashboardScreen({Key? key}) : super(key: key);
@@ -20,6 +22,8 @@ class _DashboardScreenState extends State<DashboardScreen> {
         body: _buildBody(),
         appBar: AppBar(
           titleSpacing: 0,
+          backgroundColor: Get.theme.colorScheme.background,
+          surfaceTintColor: Get.theme.colorScheme.background,
           title: Container(
             width: Get.width,
             decoration: BoxDecoration(
@@ -63,6 +67,39 @@ class _DashboardScreenState extends State<DashboardScreen> {
                     decoration: TextDecoration.underline,
                   ),
                 ],
+              ),
+              const SizedBox(
+                height: 4 * 6,
+              ),
+              GFCarousel(
+                hasPagination: true,
+                autoPlay: true,
+                activeIndicator: Colors.white,
+                autoPlayInterval: Duration(seconds: 1),
+                viewportFraction: 1.0,
+                  items: imageList.map((url) {
+                    return SizedBox(
+                      // margin: EdgeInsets.all(8.0),
+                      width: Get.width,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                        child: Image.network(url,
+                            fit: BoxFit.cover, width: 1000.0),
+                      ),
+                    );
+                  }).toList()),
+              Container(
+                height: 400,
+                color: Colors.red,
+                child: Stack(
+                  children: [
+                    imageNetwork(
+                        url:
+                            'https://i0.wp.com/post.healthline.com/wp-content/uploads/2021/04/barbell-lift-squat-1296x728-header.jpg?w=1155&h=1528',
+                        fit: BoxFit.cover,
+                        height: double.infinity)
+                  ],
+                ),
               )
             ],
           ),
@@ -71,3 +108,10 @@ class _DashboardScreenState extends State<DashboardScreen> {
     ));
   }
 }
+
+final List<String> imageList = [
+  "https://cdn.pixabay.com/photo/2017/12/03/18/04/christmas-balls-2995437_960_720.jpg",
+  "https://cdn.pixabay.com/photo/2017/12/13/00/23/christmas-3015776_960_720.jpg",
+  "https://cdn.pixabay.com/photo/2019/12/19/10/55/christmas-market-4705877_960_720.jpg",
+  "https://cdn.pixabay.com/photo/2019/12/20/00/03/road-4707345_960_720.jpg",
+];
