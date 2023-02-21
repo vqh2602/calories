49cd44c5cd69f26fbf76db94447fc880c1a7d2f3
vuong quang huy <vqh2602@gmail.com>
2023-02-21 21:15:11 +0700

    ui 50%

--- lib/modules/dashboard/dashboard_screen.dart
+++ lib/modules/dashboard/dashboard_screen.dart
@@ -5,6 +5,7 @@ import 'package:calories/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:getwidget/getwidget.dart';
+import 'package:lucide_icons/lucide_icons.dart';
 
 class DashboardScreen extends StatefulWidget {
   const DashboardScreen({Key? key}) : super(key: key);
@@ -39,74 +40,120 @@ class _DashboardScreenState extends State<DashboardScreen> {
   Widget _buildBody() {
     return SafeArea(
         child: Container(
-      margin: alignment_20_0(),
-      child: Container(
-        padding: EdgeInsets.zero,
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(
-                height: 4 * 3,
-              ),
-              textHeadlineSmall(text: 'Thống kê'),
-              const SizedBox(
-                height: 4 * 6,
-              ),
-              blockStatisticalToday(),
-              const SizedBox(
-                height: 4 * 14,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  textHeadlineSmall(text: 'Blog tập luyện'),
-                  textBodySmall(
-                    text: 'xem thêm',
-                    decoration: TextDecoration.underline,
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 4 * 6,
-              ),
-              GFCarousel(
-                  hasPagination: true,
-                  autoPlay: true,
-                  activeIndicator: Colors.white,
-                  autoPlayInterval: const Duration(seconds: 1),
-                  viewportFraction: 1.0,
-                  items: imageList.map((url) {
-                    return SizedBox(
-                      // margin: EdgeInsets.all(8.0),
-                      width: Get.width,
-                      child: ClipRRect(
-                        borderRadius:
-                            const BorderRadius.all(Radius.circular(5.0)),
-                        child: Image.network(url,
-                            fit: BoxFit.cover, width: 1000.0),
+          padding: EdgeInsets.zero,
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  margin: alignment_20_0(),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const SizedBox(
+                        height: 4 * 3,
                       ),
-                    );
-                  }).toList()),
-              Container(
-                height: 400,
-                color: Colors.red,
-                child: Stack(
-                  children: [
-                    imageNetwork(
-                        url:
-                            'https://i0.wp.com/post.healthline.com/wp-content/uploads/2021/04/barbell-lift-squat-1296x728-header.jpg?w=1155&h=1528',
-                        fit: BoxFit.cover,
-                        height: double.infinity)
-                  ],
+                      textHeadlineSmall(text: 'Thống kê'),
+                      const SizedBox(
+                        height: 4 * 6,
+                      ),
+                      blockStatisticalToday(),
+                      const SizedBox(
+                        height: 4 * 14,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          textHeadlineSmall(text: 'Blog tập luyện'),
+                          textBodySmall(
+                            text: 'xem thêm',
+                            decoration: TextDecoration.underline,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
-              )
-            ],
+                const SizedBox(
+                  height: 4 * 6,
+                ),
+                GFCarousel(
+                    hasPagination: true,
+                    autoPlay: true,
+                    height: 450,
+                    activeIndicator: Colors.white,
+                    autoPlayInterval: const Duration(seconds: 4),
+                    viewportFraction: 1.0,
+                    items: imageList.map((url) {
+                      return SizedBox(
+                        // color: Colors.red,
+                        height: 450,
+                        child: Stack(
+                          children: [
+                            ClipRRect(
+                              borderRadius:
+                                  const BorderRadius.all(Radius.circular(5.0)),
+                              child: imageNetwork(
+                                  url: url,
+                                  fit: BoxFit.cover,
+                                  height: double.infinity),
+                            ),
+                            Container(
+                              color: Colors.black.withOpacity(0.4),
+                              width: double.infinity,
+                              height: double.infinity,
+                            ),
+                            Align(
+                              alignment: Alignment.bottomLeft,
+                              child: Container(
+                                  decoration: const BoxDecoration(
+                                      color: Colors.transparent),
+                                  margin: alignment_20_0(),
+                                  padding: const EdgeInsets.only(bottom: 4*7),
+                                  child: Wrap(
+                                    runSpacing: 12,
+                                    children: [
+                                      textHeadlineMedium(
+                                          text: 'Làm sao để tập vùng mông hiệu quả'
+                                              .toUpperCase(),
+                                          fontWeight: FontWeight.w700,
+                                          maxLines: 3,
+                                          overflow: TextOverflow.ellipsis,
+                                          color: Colors.white),
+                                      ElevatedButton(
+                                          //icon: Icon(Icons.edit, size: 32),
+                                          onPressed: () {},
+                                          style:  ElevatedButton.styleFrom(
+                                            shape: const RoundedRectangleBorder(
+                                              borderRadius: BorderRadius.zero, // <-- Radius
+                                            ),
+                                            backgroundColor: Colors.white,
+                                            padding: const EdgeInsets.all(4*3)
+                                          ),
+                                          child: Wrap(
+                                            crossAxisAlignment: WrapCrossAlignment.center,
+                                            children: [
+                                              textTitleLarge(
+                                                  text: 'Đọc chi tiết',
+                                                  textAlign: TextAlign.center,
+                                                  fontWeight: FontWeight.w500,
+                                                  color: Colors.black),
+                                              const SizedBox(width: 8,),
+                                              const Icon(LucideIcons.arrowRight,color: Colors.black,)
+                                            ],
+                                          ),),
+                                    ],
+                                  )),
+                            )
+                          ],
+                        ),
+                      );
+                    }).toList()),
+              ],
+            ),
           ),
-        ),
-      ),
-    ));
+        ));
   }
 }
 
